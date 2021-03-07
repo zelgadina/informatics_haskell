6793bac1a0213d9c20fb6cfed13b3a5988177ebc
zelgadina <zelgadina@yandex.ru>
2021-03-07 23:14:33 +0300

    fix N task

--- if/N.hs
+++ if/N.hs
@@ -6,7 +6,7 @@ main = do
 equation :: Int -> Int -> String
 equation a b
     | (a == 0) && (b == 0) = "INF"
-    | a == 0 && b /= 0     = "NO"
+    | a == 0               = "NO"
     | (0 - b) `mod` a == 0 = root
     | otherwise            = "NO"
     where root = show $ (0 - b) `div` a
